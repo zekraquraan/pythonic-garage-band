3c5acc59695090015a11e20081974a1375855a0b
zekraquraan <zekraquraan7@gmail.com>
2023-05-23 21:16:04 +0300

    9:15

--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -1,8 +1,25 @@
 class Musician:
+    """
+    A base class representing a musician.
+    """
+
     def play_solo(self):
+        """
+        Play a solo.
+
+        This method should be overridden by subclasses.
+        """
         pass
 
+
 class Guitarist(Musician):
+    """
+    A class representing a guitarist.
+
+    Attributes:
+        name (str): The name of the guitarist.
+    """
+
     def __init__(self, name):
         self.name = name
 
@@ -13,12 +30,32 @@ class Guitarist(Musician):
         return f"Guitarist instance. Name = {self.name}"
 
     def get_instrument(self):
+        """
+        Get the instrument played by the guitarist.
+
+        Returns:
+            str: The instrument (guitar).
+        """
         return "guitar"
+
     def play_solo(self):
+        """
+        Play a face-melting guitar solo.
+
+        Returns:
+            str: The guitar solo.
+        """
         return "face melting guitar solo"
 
 
 class Drummer(Musician):
+    """
+    A class representing a drummer.
+
+    Attributes:
+        name (str): The name of the drummer.
+    """
+
     def __init__(self, name):
         self.name = name
 
@@ -29,12 +66,32 @@ class Drummer(Musician):
         return f"Drummer instance. Name = {self.name}"
 
     def get_instrument(self):
+        """
+        Get the instrument played by the drummer.
+
+        Returns:
+            str: The instrument (drums).
+        """
         return "drums"
+
     def play_solo(self):
+        """
+        Play a rattle boom crash drum solo.
+
+        Returns:
+            str: The drum solo.
+        """
         return "rattle boom crash"
 
 
 class Bassist(Musician):
+    """
+    A class representing a bassist.
+
+    Attributes:
+        name (str): The name of the bassist.
+    """
+
     def __init__(self, name):
         self.name = name
 
@@ -45,26 +102,66 @@ class Bassist(Musician):
         return f"Bassist instance. Name = {self.name}"
 
     def get_instrument(self):
+        """
+        Get the instrument played by the bassist.
+
+        Returns:
+            str: The instrument (bass).
+        """
         return "bass"
+
     def play_solo(self):
+        """
+        Play a bom bom buh bom bass solo.
+
+        Returns:
+            str: The bass solo.
+        """
         return "bom bom buh bom"
 
 
 class Band:
+    """
+    A class representing a band.
+
+    Attributes:
+        name (str): The name of the band.
+        members (list): A list of members in the band.
+    """
+
     instances = []
-    def __init__(self, name, members=[]):
+
+    def __init__(self, name, members=None):
         self.name = name
-        self.members = members 
+        self.members = members or []
         Band.instances.append(self)
 
     @classmethod
     def to_list(cls):
+        """
+        Get a list of all band instances.
+
+        Returns:
+            list: A list of band instances.
+        """
         return cls.instances
 
     def add_member(self, member):
+        """
+        Add a member to the band.
+
+        Args:
+            member (Musician): The musician to add to the band.
+        """
         self.members.append(member)
 
     def play_solos(self):
+        """
+        Play solos for each band member.
+
+        Returns:
+            list: A list of solos played by each band member.
+        """
         solos = []
         for member in self.members:
             solo = member.play_solo()
--- tests/test_band.py
+++ tests/test_band.py
@@ -9,7 +9,7 @@ from pythonic_garage_band.pythonic_garage_band import (
 )
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_guitarist_str():
     joan = Guitarist("Joan Jett")
     actual = str(joan)
@@ -17,7 +17,7 @@ def test_guitarist_str():
     assert actual == expected
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_guitarist_repr():
     joan = Guitarist("Joan Jett")
     actual = repr(joan)
@@ -25,7 +25,7 @@ def test_guitarist_repr():
     assert actual == expected
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_drummer_str():
     sheila = Drummer("Sheila E.")
     actual = str(sheila)
@@ -33,7 +33,7 @@ def test_drummer_str():
     assert actual == expected
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_drummer_repr():
     sheila = Drummer("Sheila E.")
     actual = repr(sheila)
@@ -41,7 +41,7 @@ def test_drummer_repr():
     assert actual == expected
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_bassist_str():
     meshell = Bassist("Meshell Ndegeocello")
     actual = str(meshell)
@@ -49,7 +49,7 @@ def test_bassist_str():
     assert actual == expected
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_bassist_repr():
     meshell = Bassist("Meshell Ndegeocello")
     actual = repr(meshell)
@@ -57,35 +57,35 @@ def test_bassist_repr():
     assert actual == expected
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_band_name():
     nirvana = Band("Nirvana", [])
 
     assert nirvana.name == "Nirvana"
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_guitarist():
     jimi = Guitarist("Jimi Hendrix")
     assert jimi.name == "Jimi Hendrix"
     assert jimi.get_instrument() == "guitar"
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_bassist():
     flea = Bassist("Flea")
     assert flea.name == "Flea"
     assert flea.get_instrument() == "bass"
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_drummer():
     ginger = Drummer("Ginger Baker")
     assert ginger.name == "Ginger Baker"
     assert ginger.get_instrument() == "drums"
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_instruments(one_band):
     instruments = ["guitar", "bass", "drums"]
     for i, member in enumerate(one_band.members):
@@ -93,7 +93,7 @@ def test_instruments(one_band):
         assert member.get_instrument() == instruments[i]
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_individual_solos(one_band):
     for member in one_band.members:
         if member.get_instrument() == "guitar":
@@ -104,7 +104,7 @@ def test_individual_solos(one_band):
             assert member.play_solo() == "rattle boom crash"
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_band_members(one_band):
 
     assert len(one_band.members) == 3
@@ -122,7 +122,7 @@ def test_band_members(one_band):
     assert one_band.members[2].name == "Dave Grohl"
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_play_solos_for_whole_band(one_band):
     solos = one_band.play_solos()
     assert len(solos) == 3
@@ -131,7 +131,7 @@ def test_play_solos_for_whole_band(one_band):
     assert solos[2] == "rattle boom crash"
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_class_tracks_instances():
     assert Band.to_list() == []
     the_nobodies = Band("The Nobodies", [])
@@ -139,7 +139,7 @@ def test_class_tracks_instances():
     assert Band.instances[0] == the_nobodies
 
 
-# @pytest.mark.skip("todo")
+@pytest.mark.todo
 def test_to_list():
     assert Band.to_list() == []
     the_nobodies = Band("The Nobodies", [])
@@ -148,9 +148,9 @@ def test_to_list():
     assert all_bands[0] == the_nobodies
 
 
-# #######################
-# # Fixtures
-# #######################
+#######################
+# Fixtures
+#######################
 
 
 @pytest.fixture
@@ -187,3 +187,48 @@ def clean():
     Check the docs for that. Hint: it uses yield
     """
     Band.instances = []
+
+
+#######################
+# Stretch
+#######################
+
+
+# @pytest.mark.skip("stretch")
+# def test_from_file():
+#     with open("assets/bands.json") as f:
+#         bands = json.loads(f.read())
+
+#     assert len(bands) == 1
+
+#     nirvana_data = bands[0]
+
+#     nirvana = Band(nirvana_data["name"], nirvana_data["members"])
+
+#     assert nirvana.name == "Nirvana"
+
+
+# @pytest.mark.skip("stretch")
+# def test_from_yaml():
+#     bands = yaml.safe_load(open("assets/bands.yml"))
+
+#     assert bands[0]["name"] == "Nirvana"
+
+#     assert bands[1]["name"] == "The Pixies"
+
+
+# @pytest.mark.skip("stretch")
+# def test_abstract_musician():
+#     with pytest.raises(TypeError):
+#         Musician("nobody", "nothing", "silence")
+
+
+# @pytest.mark.skip("stretch")
+# def test_incomplete_keyboardist():
+#     with pytest.raises(TypeError) as e:
+#         Keyboardist("Booker T. Jones")
+
+#     assert (
+#         repr(e)
+#         == """<ExceptionInfo TypeError("Can't instantiate abstract class Keyboardist with abstract method some_method_that_must_be_implemented_in_base_class") tblen=1>"""  # noqa: E501
+#     )
